fc563e31795d16c2f0cb06a07247fa8049dc06a7
Sathish Kumar G <105732907+Sathish020700@users.noreply.github.com>
2023-04-27 11:15:18 +0530

    Updated 3 answers
    Lines: 43 - 57

--- Basic Select/SQL File Answer.sql
+++ Basic Select/SQL File Answer.sql
@@ -40,5 +40,18 @@ select count(*) - count(distinct CITY) from STATION;
 union
 (select CITY, length(CITY) from STATION order by length(CITY) asc,CITY  limit 1);
 
+# Weather Observation Station 6
 
+select distinct CITY from STATION where CITY like 'A%' or CITY like 'E%' or CITY like 'I%' or CITY like 'O%' or CITY like 'U%';
+                                                      (or)
+select distinct CITY from STATION where lower(substr(CITY,1,1)) in ('a','e','i','o','u');
 
+# Weather Observation Station 7
+
+select distinct CITY from STATION where CITY like '%a' or CITY like '%e' or CITY like '%i' or CITY like '%o' or CITY like '%u';
+                                                      (or)
+select distinct CITY from STATION where lower(substr(CITY,-1,1)) in ("a","e","i","o","u");
+
+# Weather Observation Station 8
+
+select distinct CITY from STATION where substr(CITY,1,1) in ('A','E','I','O','U') and substr(CITY,-1,1) in ('a','e','i','o','u');;
