e3115b3be08c68ecdd7351a938ed8a205c31ed7d
Sathish Kumar G <105732907+Sathish020700@users.noreply.github.com>
2023-05-01 20:02:08 +0530

    added 2 answers
    Lines : 5 - 11

--- Aggregation/SQL File Answer.sql
+++ Aggregation/SQL File Answer.sql
@@ -1,3 +1,11 @@
 # Revising Aggregations - The Count Function
 
 select count(*) from CITY where POPULATION > 100000;
+
+# Revising Aggregations - The Sum Function
+
+select sum(POPULATION) from CITY where DISTRICT = "California";
+
+# Revising Aggregations - Averages
+
+select avg(POPULATION) from CITY where DISTRICT = "California";
