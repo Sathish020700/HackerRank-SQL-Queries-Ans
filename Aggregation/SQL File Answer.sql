72197f4c48c0be696c43c72f5e20c645e6bc2bea
Sathish Kumar G <105732907+Sathish020700@users.noreply.github.com>
2023-05-02 08:48:51 +0530

    Added 5 answers
    Lines: 13 - 33

--- Aggregation/SQL File Answer.sql
+++ Aggregation/SQL File Answer.sql
@@ -9,3 +9,26 @@ select sum(POPULATION) from CITY where DISTRICT = "California";
 # Revising Aggregations - Averages
 
 select avg(POPULATION) from CITY where DISTRICT = "California";
+
+# Average Population
+
+select floor(avg(POPULATION)) from CITY;
+                     (or)
+select round(avg(POPULATION)) from CITY;
+
+# Japan Population
+
+select sum(POPULATION) from CITY where COUNTRYCODE = "JPN";
+
+# Population Density Difference
+
+select (max(POPULATION)-min(POPULATION)) from CITY;
+
+# The Blunder
+
+select ceil(avg(Salary)- avg(replace(Salary,'0',''))) from EMPLOYEES;
+
+# Top Earners
+
+select salary*months as maximum_salary, count(*) from Employee group by salary*months order by maximum_salary desc limit 1;
+
