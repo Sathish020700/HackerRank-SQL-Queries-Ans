3d06c3d1c6a0072b7f5ce7cb9118283ef9174673
Sathish Kumar G <105732907+Sathish020700@users.noreply.github.com>
2023-05-03 09:58:56 +0530

    updated 6 answers
    Lines: 35 - 61

--- Aggregation/SQL File Answer.sql
+++ Aggregation/SQL File Answer.sql
@@ -32,3 +32,31 @@ select ceil(avg(Salary)- avg(replace(Salary,'0',''))) from EMPLOYEES;
 
 select salary*months as maximum_salary, count(*) from Employee group by salary*months order by maximum_salary desc limit 1;
 
+# Weather Observation Station 2
+
+select round(sum(LAT_N),2), round(sum(LONG_W),2) from STATION;
+
+# Weather Observation Station 13
+
+select round(sum(LAT_N),4) from STATION where round(LAT_N,4) between 38.7880 and 137.2345;
+
+# Weather Observation Station 14
+
+select max(round(LAT_N,4)) from STATION where round(LAT_N,4) < 137.2345;
+
+# Weather Observation Station 15
+
+select round(LONG_W,4) from STATION where LAT_N = (select max(LAT_N) from STATION where LAT_N < 137.2345);
+                                              (or)
+select round(LONG_W,4) from STATION where LAT_N < 137.2345 order by LAT_N desc limit 1;
+
+# Weather Observation Station 16
+
+select round(LAT_N,4) from STATION where LAT_N > 38.7780 order by LAT_N asc limit 1;
+                                           (or)
+select round(min(LAT_N),4) from STATION where LAT_N > 38.7780;
+
+# Weather Observation Station 17
+
+select round(LONG_W,4) from STATION where LAT_N >38.7780 order by LAT_N asc limit 1; -- also can be written using sub query as in case of # Weather Observation Station 15
+
