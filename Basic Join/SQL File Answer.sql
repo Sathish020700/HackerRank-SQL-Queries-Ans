becb795be70753d17f461551613d0d0d3758d00f
Sathish Kumar G <105732907+Sathish020700@users.noreply.github.com>
2023-05-04 07:41:24 +0530

    updated 3 answers
    Lines: 1 - 11

--- Basic Join/SQL File Answer.sql
+++ Basic Join/SQL File Answer.sql
@@ -1,1 +1,11 @@
+# Population Census
 
+select sum(CITY.POPULATION) from CITY join COUNTRY on CITY.COUNTRYCODE = COUNTRY.CODE where CONTINENT = "Asia";   --instead of join, inner join is also the same.
+
+# African Cities
+
+select C1.NAME from CITY C1 inner join COUNTRY C2 on C1.COUNTRYCODE = C2.CODE where CONTINENT = "Africa";
+
+# Average Population of Each Continent
+
+select C2.CONTINENT, floor(avg(C1.POPULATION)) from CITY C1 join COUNTRY C2 on C1.COUNTRYCODE = C2.CODE group by C2.CONTINENT;
