f1b08898e953a4b02069effdfac7778c813aeac4
Sathish Kumar G <105732907+Sathish020700@users.noreply.github.com>
2023-05-15 10:38:55 +0530

    Added 1 ans
    Lines: 13 - 18

--- Basic Join/SQL File Answer.sql
+++ Basic Join/SQL File Answer.sql
@@ -9,3 +9,10 @@ select C1.NAME from CITY C1 inner join COUNTRY C2 on C1.COUNTRYCODE = C2.CODE wh
 # Average Population of Each Continent
 
 select C2.CONTINENT, floor(avg(C1.POPULATION)) from CITY C1 join COUNTRY C2 on C1.COUNTRYCODE = C2.CODE group by C2.CONTINENT;
+
+# The Report
+
+select case 
+    when Grade>=8 then S.Name 
+    else NULL 
+    end as Name, Grade, Marks from Students S inner join Grades G on S.Marks between G.Min_Mark and G.Max_Mark order by G.Grade desc, S.Name, S.Marks;
